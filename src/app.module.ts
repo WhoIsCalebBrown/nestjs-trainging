d4453f374ceec88f145f661f8fcafbe8f2b0b49c
Caleb Brown <caleb.brown9999@gmail.com>
2023-05-25 08:39:36 -0400

    added service/controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
+import { TaksService } from './taks/taks.service';
 
 @Module({
   imports: [TasksModule],
+  providers: [TaksService],
 })
 export class AppModule {}
